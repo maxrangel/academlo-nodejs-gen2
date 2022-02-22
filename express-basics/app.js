4f2a46c4f314ac34386c3d58571b43af08e3f7b9
maxrangel <lik4ever@hotmail.com>
2022-02-21 21:51:04 -0600

    created a bettter folder structure

--- express-basics/app.js
+++ express-basics/app.js
@@ -1,16 +1,8 @@
 const express = require('express');
 
-const users = [
-	{ name: 'Max', age: 23 },
-	{ name: 'John', age: 22 },
-	{ name: 'Jill', age: 21 },
-];
-
-const posts = [
-	{ id: 1, title: 'Post 1', content: 'Some content', author: 'Max' },
-	{ id: 2, title: 'Post 2', content: 'Some content 2', author: 'John' },
-	{ id: 3, title: 'Post 3', content: 'Some content 3', author: 'Jill' },
-];
+// Routers
+// const postsRouter = require('./routes/posts.routes');
+const { postsRouter } = require('./routes/posts.routes');
 
 // Init express app
 const app = express();
@@ -18,168 +10,10 @@ const app = express();
 // Enable JSON incoming data
 app.use(express.json());
 
-const filterObj = (obj, ...allowedFields) => {
-	// allowedFields = ['title', 'content', 'author']
-	// obj = { title: 'New title', content: 'New content', email, comment }
-
-	const newObj = {};
-
-	// Get the obj properties [title, content, email, comment]
-	Object.keys(obj).forEach(el => {
-		if (allowedFields.includes(el)) {
-			newObj[el] = obj[el];
-		}
-	});
-
-	return newObj;
-};
-
 // Endpoints
-// GET http://localhost:4000/users
-app.get('/users', (req, res) => {
-	res.json({ status: 'success', data: { users } });
-});
-
-// GET http://localhost:4000/posts
-app.get('/posts', (req, res) => {
-	res.status(200).json({
-		status: 'success',
-		data: {
-			posts,
-		},
-	});
-});
-
-// GET http://localhost:4000/posts/:id
-app.get('/posts/:id', (req, res) => {
-	const { id } = req.params;
-
-	const post = posts.find(post => post.id === +id);
-
-	if (!post) {
-		res.status(404).json({
-			status: 'error',
-			message: 'No post found with the given ID',
-		});
-		return;
-	}
-
-	res.status(200).json({
-		status: 'success',
-		data: {
-			post,
-		},
-	});
-});
-
-// POST http://localhost:4000/posts
-app.post('/posts', (req, res) => {
-	const { title, content } = req.body;
-
-	const newPost = {
-		id: Math.floor(Math.random() * 100),
-		title,
-		content,
-	};
-
-	posts.push(newPost);
-
-	res.status(201).json({
-		status: 'success',
-		data: { newPost },
-	});
-});
-
-// PUT http://localhost:4000/posts/:id
-app.put('/posts/:id', (req, res) => {
-	const { id } = req.params;
-	const { title, content, author } = req.body;
-
-	// Validate the data has some value
-	if (
-		!title ||
-		!content ||
-		!author ||
-		title.length === 0 ||
-		content.length === 0 ||
-		author.length === 0
-	) {
-		res.status(400).json({
-			status: 'error',
-			message: 'Must provide a title, content and the author for this reuqest',
-		});
-		return;
-	}
-
-	// Find post by id, and get the index
-	const postIndex = posts.findIndex(post => post.id === +id);
-
-	if (postIndex === -1) {
-		res.status(404).json({
-			status: 'error',
-			message: 'Cant update post, invalid ID',
-		});
-		return;
-	}
-
-	// Update post and save it in the list
-	const updatePost = posts[postIndex];
-
-	updatePost.title = title;
-	updatePost.content = content;
-	updatePost.author = author;
-
-	posts[postIndex] = updatePost;
-
-	// 204 - No content
-	res.status(204).json({ status: 'success' });
-});
-
-// PATCH http://localhost:4000/posts/:id
-app.patch('/posts/:id', (req, res) => {
-	const { id } = req.params;
-
-	const data = filterObj(req.body, 'title', 'content', 'author');
-
-	const postIndex = posts.findIndex(post => post.id === +id);
-
-	if (postIndex === -1) {
-		res.status(404).json({
-			status: 'error',
-			message: 'Cant update post, invalid ID',
-		});
-		return;
-	}
-
-	let updatedPost = posts[postIndex];
-
-	updatedPost = { ...updatedPost, ...data };
-
-	posts[postIndex] = updatedPost;
-
-	res.status(204).json({ status: 'success' });
-});
-
-// DELETE http://localhost:4000/posts/:id
-app.delete('/posts/:id', (req, res) => {
-	const { id } = req.params;
-
-	// Find post index, by the given id
-	const postIndex = posts.findIndex(post => post.id === +id);
-
-	if (postIndex === -1) {
-		res.status(404).json({
-			status: 'error',
-			message: 'Cant delete post, invalid ID',
-		});
-		return;
-	}
-
-	// Use splice to delete post
-	posts.splice(postIndex, 1);
-
-	res.status(204).json({ status: 'success' });
-});
+// http://localhost:4000/api/v1/posts
+app.use('/api/v1/posts', postsRouter);
+app.use('/api/v1/users');
 
 app.listen(4000, () => {
 	console.log('Express app running');
---
+++ express-basics/controllers/users.controller.js
@@ -0,0 +1,11 @@
+const users = [
+	{ id: 1, name: 'Max', age: 23 },
+	{ id: 2, name: 'John', age: 22 },
+	{ id: 3, name: 'Jill', age: 21 },
+];
+
+// Get all users
+// Get user by ID
+// Save new user
+// Update user (patch)
+// Delete user
