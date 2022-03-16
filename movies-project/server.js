5594c23002c7029eb8ed46258de42bf08ca872ef
maxrangel <lik4ever@hotmail.com>
2022-03-16 00:02:54 -0600

    resolved half of instructions

---
+++ movies-project/server.js
@@ -0,0 +1,15 @@
+const { app } = require('./app');
+
+// Utils
+const { sequelize } = require('./util/database');
+
+sequelize
+  .authenticate()
+  .then(() => console.log('Database authenticated'))
+  .catch((err) => console.log(err));
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Express app running on port: ${PORT}`);
+});
